be39d96337a9dcb210b0c2bb8c9cbedd247d2820
umiskky <umiskky@outlook.com>
2021-04-14 18:37:28 +0800

    First Commit

---
+++ src/test/java/com/umiskky/model/domain/GroupTest.java
@@ -0,0 +1,17 @@
+package com.umiskky.model.domain;
+
+import org.junit.Test;
+
+/**
+ * @author umiskky
+ * @version 0.0.1
+ * @date 2021/04/14
+ */
+public class GroupTest {
+
+    @Test
+    public void testGroup(){
+        System.out.println("Test");
+    }
+
+}
---
+++ src/test/java/com/umiskky/model/domain/MessageTest.java
@@ -0,0 +1,15 @@
+package com.umiskky.model.domain;
+
+import org.junit.Test;
+
+/**
+ * @author umiskky
+ * @version 0.0.1
+ * @date 2021/04/14
+ */
+public class MessageTest {
+    @Test
+    public void testMessage(){
+
+    }
+}
