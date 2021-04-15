d2ea70181eeca629efec7d1944539763704eb036
SoonMachine <sunamnxin@126.com>
2021-04-15 15:03:05 +0800

    try to rollback

---
+++ src/main/java/org/umiskky/model/job/server/MessageHandle.java
@@ -0,0 +1,17 @@
+package org.umiskky.model.job.server;
+
+import org.umiskky.model.domain.Message;
+
+/**
+ * 处理message的线程
+ */
+
+public class MessageHandle{
+    private  Message message;
+    public void handle(){
+/*        while (true){
+            if (!MessageBuffer.messageLinkedList.isEmpty())
+        message = MessageBuffer.messageLinkedList.peekFirst();
+        }*/
+    }
+}
---
+++ src/main/java/org/umiskky/model/utils/SocketUtil.java
@@ -0,0 +1,33 @@
+package org.umiskky.model.utils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @apiNote Socket操作相关工具类
+ */
+
+public class SocketUtil {
+    /** 关闭Socket */
+    public static void close(Socket socket) {
+        if (socket != null && !socket.isClosed()) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /** 关闭ServerSocket */
+    public static void close(ServerSocket ss) {
+        if (ss != null && !ss.isClosed()) {
+            try {
+                ss.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
